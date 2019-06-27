12af615568c24a50ffab3e388302f8a3198b3822
Victor Cordova <v.cordova@mytaxi.com>
2019-06-27 10:51:03 +0200

    chore: Add dist/ folder for usage from github

---
+++ dist/shared/utils.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "callIfDefined", {
+  enumerable: true,
+  get: function get() {
+    return _utils.callIfDefined;
+  }
+});
+
+var _utils = require("react-date-picker/dist/shared/utils");
